9d0c4cb234ce726113e7f5d1d3ac78a774cffd20
Pål Bergerskogen <pal.bergerskogen@gmail.com>
2016-03-07 14:12:39 +0100

    SQL script now creates database corresponding to ER diagram

---
+++ WorkoutDiary.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class WorkoutDiary {
+    public static String getGoal()
+    
+
+
+    public static void main(String args[]){
+        return 0;
+    }
+
+}

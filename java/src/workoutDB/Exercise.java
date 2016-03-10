3090b798f4486e5695e53d21b1be942df0b80c18
svennhv <svennhv@gmail.com>
2016-03-10 18:48:49 +0100

    created init() and terminate() class:Exercise

--- java/src/workoutDB/Exercise.java
+++ java/src/workoutDB/Exercise.java
@@ -2,6 +2,20 @@ package workoutDB;
 
 public class Exercise {
 	
+	protected static void init(){
+		  //STEP 3: Open a connection
+		  System.out.println("Connecting to database...");
+		  conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		  //STEP 4: Execute a query
+		  System.out.println("Exercise: Creating statement...");
+		  stmt = conn.createStatement();
+	}
+	
+	protected static void terminate(){
+		  stmt.close();
+		  conn.close();
+	}
 	
 	protected static String getAll(){
 		

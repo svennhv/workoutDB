af8a4b995b46efc45a77a97819dbce586d29260f
svennhv <svennhv@gmail.com>
2016-03-15 13:18:10 +0100

    clean up

--- java/src/workoutDB/Exercise.java
+++ java/src/workoutDB/Exercise.java
@@ -1,28 +1,13 @@
 package workoutDB;
 
 public class Exercise {
-	
-	protected static void init(){
-		  //STEP 3: Open a connection
-		  System.out.println("Connecting to database...");
-		  conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
-		  //STEP 4: Execute a query
-		  System.out.println("Exercise: Creating statement...");
-		  stmt = conn.createStatement();
+	public Exercise(){
+
 	}
-	
-	protected static void terminate(){
-		  stmt.close();
-		  conn.close();
-	}
-	
-	protected static String getAll(){
-		
-	}
-	
-	
-	// only use in class:
-	
-	private 
+
+
 }
+
+
+	// only use in class:

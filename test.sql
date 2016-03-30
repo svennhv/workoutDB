e1d418534cdca7b21ef4c74089f72152b2a67b24
Tomas Slagstad <tomasl@hotmail.no>
2016-03-30 22:19:09 +0200

    updated test.sql

--- test.sql
+++ test.sql
@@ -1,18 +1,18 @@
 
 INSERT INTO EXERCISE
-VALUES( "bench press", "breast exercise", "120kg x 3", "100kg x3", "70", "5", "3", NULL, NULL);
+VALUES( "bench press", "breast exercise", "120kg x 3", "100kg x3", 70, 5, 3, NULL, NULL);
 
 INSERT INTO EXERCISE
-VALUES( "squats", "leg exercise", "160kg x 3", "120kg x3", "90", "5", "3", NULL, NULL);
+VALUES( "squats", "leg exercise", "160kg x 3", "120kg x3", 90, 5, 3, NULL, NULL);
 
 INSERT INTO EXERCISE
-VALUES("jogging", "endurence training", "10km", "12 km", NULL,  NULL, NULL, "10km", "65 min");
+VALUES("jogging", "endurence training", "10km", "12 km", NULL,  NULL, NULL, 10, 65);
 
 INSERT INTO WORKOUT
-VALUES(NULL, "Strength exercise", TRUE, "16.03.2017", 60, "average", "good", "upper body strength exercises", NULL, "good", "none");
+VALUES(NULL, "Strength exercise", TRUE, "16.03.2017", 60, 6, 8, "upper body strength exercises", NULL, "good", 0);
 
 INSERT INTO WORKOUT
-VALUES(NULL, "Endurance ecercise", TRUE, "17.03.2017", 70, "average", "average", "endurance exercise", "sunny, warm temperature", NULL,NULL);
+VALUES(NULL, "Endurance ecercise", TRUE, "17.03.2017", 70, 6, 6, "endurance exercise", "sunny, warm temperature", NULL,NULL);
 
 INSERT INTO WORKOUTTEMPLATE
 VALUES(1,1);
